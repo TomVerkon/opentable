3c5be15dedead599c7204d12a2e64b1b9e310d96
TomVerkon <tverkon@gmail.com>
2023-09-03 13:42:35 -0400

    slight mod on mainnavbar

--- app/components/MainNavBar.tsx
+++ app/components/MainNavBar.tsx
@@ -9,10 +9,6 @@ function MainNavBar() {
   const { data, loading } = useContext(AuthenticationContext);
   const { signout } = useAuth();
 
-  const handleLogout = () => {
-    signout();
-  };
-
   return (
     <nav className="bg-white p-2 flex justify-between">
       <Link href="/" className="font-bold text-gray-700 text-2xl">
@@ -25,9 +21,9 @@ function MainNavBar() {
             {data ? (
               <button
                 className="bg-blue-400 text-white border p-1 px-4 rounded mr-3"
-                onClick={handleLogout}
+                onClick={signout}
               >
-                Logout
+                Signout
               </button>
             ) : (
               <>
